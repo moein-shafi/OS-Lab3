853ed96113fbf84ea4b771ca7f7ad1c41c282f1d
Ghazal <kalhorghazal1378@gmail.com>
2020-05-10 06:59:50 +0000

    Fix Lottery Scheduler.

--- proc.c
+++ proc.c
@@ -335,7 +335,7 @@ wait(void)
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
 
-struct proc*
+struct proc* 
 get_lottery_sched_proc(void)
 {
   struct proc *p;
@@ -357,15 +357,15 @@ get_lottery_sched_proc(void)
   random = ticks;
   release(&tickslock);
 
-  goal_ticket = (random) % total_tickets;
-
   if(has_proc){
+    random = ticks;
+    goal_ticket = (random) % total_tickets;
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE || p->queue_num != LOTTERY)
         continue;
 
       cur_tickets += p->ticket;
-
+    
       if(goal_ticket < cur_tickets){
         return p;
       }
@@ -461,7 +461,7 @@ check_aging()
 }
 
 void
-scheduler2(void)
+scheduler(void)
 {
   struct proc *p;
   struct cpu *c = mycpu();
@@ -512,41 +512,6 @@ scheduler2(void)
   }
 }
 
-void
-scheduler(void)
-{
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
-
-  }
-}
 // Enter scheduler.  Must hold only ptable.lock
 // and have changed proc->state. Saves and restores
 // intena because intena is a property of this
