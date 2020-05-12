c8b6fd26699e20fc0f9e6a06fc9e56e7784329a1
Ghazal <kalhorghazal1378@gmail.com>
2020-05-12 20:16:35 +0000

    Fix Bugs in Printing Float.

--- proc.c
+++ proc.c
@@ -764,6 +764,75 @@ int seprate_floating_part(double num)
   return fpart;
 }
 
+void reverse(char* str, int len) 
+{ 
+    int i = 0, j = len - 1, temp; 
+    while (i < j) { 
+        temp = str[i]; 
+        str[i] = str[j]; 
+        str[j] = temp; 
+        i++; 
+        j--; 
+    } 
+} 
+  
+// Converts a given integer x to string str[].  
+// d is the number of digits required in the output.  
+// If d is more than the number of digits in x,  
+// then 0s are added at the beginning. 
+int intToStr(int x, char str[], int d) 
+{ 
+    int i = 0; 
+    while (x) { 
+        str[i++] = (x % 10) + '0'; 
+        x = x / 10; 
+    } 
+  
+    // If number of digits required is more, then 
+    // add 0s at the beginning 
+    while (i < d) 
+        str[i++] = '0'; 
+  
+    reverse(str, i); 
+    str[i] = '\0'; 
+    return i; 
+} 
+
+int pow(int x, unsigned int y) 
+{ 
+    if (y == 0) 
+        return 1; 
+    else if (y % 2 == 0) 
+        return pow(x, y / 2) * pow(x, y / 2); 
+    else
+        return x * pow(x, y / 2) * pow(x, y / 2); 
+} 
+  
+// Converts a floating-point/double number to a string. 
+void ftoa(float n, char* res, int afterpoint) 
+{ 
+    // Extract integer part 
+    int ipart = (int)n; 
+  
+    // Extract floating part 
+    float fpart = n - (float)ipart; 
+  
+    // convert integer part to string 
+    int i = intToStr(ipart, res, 0); 
+  
+    // check for display option after point 
+    if (afterpoint != 0) { 
+        res[i] = '.'; // add dot 
+  
+        // Get the value of fraction part upto given no. 
+        // of points after dot. The third parameter  
+        // is needed to handle cases like 233.007 
+        fpart = fpart * pow(10, afterpoint); 
+  
+        intToStr((int)fpart, res + i + 1, afterpoint); 
+    } 
+}
+
 int
 print_processes(void)
 {
@@ -840,11 +909,16 @@ print_processes(void)
     print_spaces(max_column_lens[CYCLES] - count_num_of_digits(p->cycles));
     
     double hrrn_ratio = calculate_hrrn(p->arrival_time, p->cycles);
+
+    char hrrn_str[30];
+
+    ftoa(hrrn_ratio, hrrn_str, 4);
+    cprintf("%s\n", hrrn_str);
     // double hrrn_ratio = 10.3254;
-    int ipart = (int)hrrn_ratio;
-    cprintf("%d.\n", ipart);
-    cprintf("%d\n", seprate_floating_part(hrrn_ratio));
-    print_spaces(max_column_lens[HRRN_TITLE] - (3 + count_num_of_digits(ipart)));
+    //int ipart = (int)hrrn_ratio;
+    //cprintf("%d.\n", ipart);
+    //cprintf("%d\n", seprate_floating_part(hrrn_ratio));
+    //print_spaces(max_column_lens[HRRN_TITLE] - (3 + count_num_of_digits(ipart)));
     cprintf("\n");
   }
   release(&ptable.lock);
